9ef2a34a8ebe47f5c43e523bf9be6dc03ebbb38f
flp-gregorio <felipealvesgregorio01@gmail.com>
2025-05-17 07:26:56 -0300

    feat: integrate Tailwind CSS and add color palette functionality to portfolio card component

--- portfolio-page/src/app/portfolio-card/portfolio-card.component.ts
+++ portfolio-page/src/app/portfolio-card/portfolio-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ColorThiefService } from '@soarlin/angular-color-thief';
 
 @Component({
     selector: 'app-portfolio-card',
@@ -14,9 +15,21 @@ export class PortfolioCardComponent {
   @Input() imageUrl: string = '';
   @Input() projectLink: string = '';
   @Input() technologies: { name: string, color: string, icon: string }[] = [];
+  palette: [number, number, number][] | null = null;
   
   // Make window available in the template
   get window(): Window {
     return window;
+  }
+
+  constructor(private colorThief: ColorThiefService) {}
+
+  async loadPaletteFromUrl() {
+    try {
+      this.palette = await this.colorThief.getPaletteFromUrl(this.imageUrl);
+      console.log('Palette loaded:', this.palette);
+    } catch (error) {
+      console.error('Failed to load image:', error);
+    }
   }
 }
